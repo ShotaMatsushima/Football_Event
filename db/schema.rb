5de45e1e6210736741a26e30fa8db50e64d5ea9c
ShotaMatsushima <shota19890523.engineer@gmail.com>
2021-01-30 14:47:44 +0900

    add user model validation and user model test #5

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_25_080212) do
+ActiveRecord::Schema.define(version: 2021_01_29_093732) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "favorite_team", default: "", null: false
-    t.string "adress", default: "", null: false
+    t.string "address", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.index ["adress"], name: "index_users_on_adress", unique: true
+    t.index ["address"], name: "index_users_on_address", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["favorite_team"], name: "index_users_on_favorite_team", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do
   factory :user do
+    name { "shota" }
+    sequence(:email) { |n| "test#{n}@example.com" }
+    favorite_team { "Liverpool" }
+    address { "神奈川県" }
+    password { "password" }
   end
 end
