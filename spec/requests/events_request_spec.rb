b7be4426f67a39b8557d7eea5fee60760842f1b9
ShotaMatsushima <shota19890523.engineer@gmail.com>
2021-03-06 14:16:48 +0900

    change google map api in event page #34

--- spec/requests/events_request_spec.rb
+++ spec/requests/events_request_spec.rb
@@ -56,6 +56,8 @@ RSpec.describe "Events", type: :request do
       # 登録したユーザー情報が存在していること
       expect(response.body).to include event.event_name
       expect(response.body).to include event.description
+      expect(response.body).to include event.address
+      expect(response.body).to include event.title
       expect(response.body).to include event.event_start_at.to_s
       expect(response.body).to include event.event_end_at.to_s
       expect(response.body).to include event.event_team
