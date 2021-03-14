47f297076b5b9ee0da756599cb82845e11a65d24
ShotaMatsushima <shota19890523.engineer@gmail.com>
2021-03-14 17:24:13 +0900

    create comment test #43

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :event
-  validates :content, presence: true
+  validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
   validates :event_id, presence: true
 end
