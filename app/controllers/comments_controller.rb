6066d5a6bd235b4d73688d9a164bbf31800f68d9
ShotaMatsushima <shota19890523.engineer@gmail.com>
2021-03-14 09:41:23 +0900

    post comments and display #42

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,18 @@
 class CommentsController < ApplicationController
   def create
+    event = Event.find(params[:event_id])
+    @comment = event.comments.build(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      redirect_to event_path(@comment.event.id), notice: "コメントしました"
+    else
+      redirect_to event_path(@comment.event.id), alert: "コメントできませんでした"
+    end
   end
 
-  def destroy
-  end
+  private
+
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
 end
