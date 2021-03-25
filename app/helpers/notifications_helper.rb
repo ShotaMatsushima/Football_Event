10b99663e119fc649b0f76214853b032d575bbaf
ShotaMatsushima <shota19890523.engineer@gmail.com>
2021-03-25 19:28:53 +0900

    set notifications view #47

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,2 +1,15 @@
 module NotificationsHelper
+  def notification_form(notification)
+    @visiter = notification.visiter
+    @comment = nil
+    @visiter_comment = notification.comment_id
+    # notification.actionがlikeかcommentか
+    case notification.action
+      when "like" then
+        tag.a(notification.visiter.name, href:user_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたのイベント', href:event_path(notification.event_id), style:"font-weight: bold;")+"にいいねしました"
+      when "comment" then
+        @comment = Comment.find_by(id: @visiter_comment)&.content
+        tag.a(@visiter.name, href:user_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたのイベント', href:event_path(notification.event_id), style:"font-weight: bold;")+"にコメントしました"
+    end
+  end
 end
