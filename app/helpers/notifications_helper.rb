c6f0534122acbe33b59f9301a6c55fa95592ae58
ShotaMatsushima <shota19890523.engineer@gmail.com>
2021-03-26 19:36:10 +0900

    create unchecked_notifications mark #49

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -14,4 +14,8 @@ module NotificationsHelper
         tag.a(@visiter.name, href:user_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたのイベント', href:event_path(notification.event_id), style:"font-weight: bold;")+"にコメントしました"
     end
   end
+
+  def unchecked_notifications
+    @notifications = current_user.passive_notifications.where(checked: false)
+  end
 end
