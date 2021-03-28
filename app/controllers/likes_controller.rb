1d144519b906c1556ce467399359222b49a70858
ShotaMatsushima <shota19890523.engineer@gmail.com>
2021-03-28 16:01:20 +0900

    add ajax of like #52

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,15 @@
 class LikesController < ApplicationController
   def create
+    @event = Event.find(params[:event_id])
     like = current_user.likes.build(event_id: params[:event_id])
     like.save
-    redirect_to event_path(like.event.id)
     event = Event.find(params[:event_id])
     event.create_notification_like!(current_user)
-    redirect_to event_path(like.event.id)
   end
 
   def destroy
+    @event = Event.find(params[:event_id])
     like = Like.find_by(event_id: params[:event_id], user_id: current_user.id)
     like.destroy
-    redirect_to event_path(like.event.id)
   end
 end
