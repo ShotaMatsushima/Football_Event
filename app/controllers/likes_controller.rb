3c41c79c6917da9afce226869bbab30f3ed7da63
ShotaMatsushima <shota19890523.engineer@gmail.com>
2021-04-01 16:29:04 +0900

    fix pull request of likes-event

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,19 @@
 class LikesController < ApplicationController
   def create
     like = current_user.likes.build(event_id: params[:event_id])
-    like.save
-    redirect_to event_path(like.event.id)
+    if like.save
+      redirect_to event_path(like.event.id)
+    else
+      render "events/new"
+    end
   end
 
   def destroy
     like = Like.find_by(event_id: params[:event_id], user_id: current_user.id)
-    like.destroy
-    redirect_to event_path(like.event.id)
+    if like.destroy
+      redirect_to event_path(like.event.id)
+    else
+      render "events/new"
+    end
   end
 end
