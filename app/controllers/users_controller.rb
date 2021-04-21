fdaa25c5153f7d834cdf6a4e199cb7bc2032ac15
ShotaMatsushima <shota19890523.engineer@gmail.com>
2021-04-21 19:17:17 +0900

    layout of user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @participaton_events = @user.participaton_events
+    @user_events = @user.events.all.order(created_at: :desc).page(params[:page]).per(10)
+    @participaton_events = @user.participaton_events.order(created_at: :desc).page(params[:page]).per(10)
   end
 end
