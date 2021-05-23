5ab400c18ba9a59904f17468fa06bc925ff218e9
ShotaMatsushima <shota19890523.engineer@gmail.com>
2021-05-23 10:28:26 +0900

    create Ajax buttons of follow

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,15 +1,13 @@
 class RelationshipsController < ApplicationController
   def create
-    user = User.find(params[:user_id])
+    @user = User.find(params[:user_id])
     follow = current_user.active_relationships.build(follower_id: params[:user_id])
     follow.save
-    redirect_to user_path(user.id)
   end
 
   def destroy
-    user = User.find(params[:user_id])
+    @user = User.find(params[:user_id])
     follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
     follow.destroy
-    redirect_to user_path(user.id)
   end
 end
